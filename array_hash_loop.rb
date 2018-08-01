aab11c87d0e5cafaebff0bb1e79577b07fa79136
chloe alcorn <ca.alcorn@outlook.com>
2018-08-01 16:15:07 +0100

    Day 3 lab work

---
+++ array_hash_loop.rb
@@ -0,0 +1,30 @@
+
+def add_array_lengths(array_1 , array_2)
+  return array_1.length + array_2.length
+end
+
+
+def  sum_array (numbers)
+  total = 0
+  for number in numbers
+ total += number
+  end
+  return total
+end
+
+
+def find_item(array,item)
+  for house in array
+    if house == item
+      return true
+    end
+  end
+return false
+end
+
+
+def get_first_key(hash)
+  keys = hash.keys()
+  first_key =keys.first()
+  return first_key
+end
